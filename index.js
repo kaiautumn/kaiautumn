bad8e032740228dc34541b1910bb6db3a8b53513
翟秋凯 <1922044874@qq.com>
2026-03-09 20:17:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
+const fs = require("fs")
 
 const thisYear = new Date().getFullYear()
-const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
-const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
-const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
-const progressBarOfThisYear = generateProgressBar()
 
-function generateProgressBar() {
-    const progressBarCapacity = 30
-    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
-    const progressBar =
-      '█'.repeat(passedProgressBarIndex) +
-      '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
-    return `{ ${progressBar} }`
+const start = new Date(`${thisYear}-01-01T00:00:00Z`).getTime()
+const end = new Date(`${thisYear}-12-31T23:59:59Z`).getTime()
+
+const progress = (Date.now() - start) / (end - start)
+
+function bar() {
+  const size = 30
+  const done = Math.floor(progress * size)
+  return "█".repeat(done) + "▁".repeat(size - done)
 }
 
-const readme = `
-### Hi there 👋
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
----
-⏰ Updated on ${new Date().toUTCString()}
----
-![Progress Bar CI](https://github.com/kaiautumn/kaiautumn/workflows/Progress%20Bar%20CI/badge.svg)
+const progressText =
+`⏳ Year progress { ${bar()} } ${(progress*100).toFixed(2)} %`
 
-`
+let readme = fs.readFileSync("README.md","utf8")
 
-console.log(readme)
+readme = readme.replace(
+/<!-- YEAR_PROGRESS_START -->[\s\S]*<!-- YEAR_PROGRESS_END -->/,
+`<!-- YEAR_PROGRESS_START -->
+${progressText}
+<!-- YEAR_PROGRESS_END -->`
+)
+
+fs.writeFileSync("README.md", readme)
